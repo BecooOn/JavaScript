2ef4de8eb9d2fea3aadb10a0185eea58473ee335
Adam <adem.bekci.28@gmail.com>
2024-03-19 20:54:43 +0300

    dist shop

--- Projects/06-M1-Shopping/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-// import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-    // plugins: [react()],
-    base:"/JavaScript/Projects/06-M1-Shopping/"
-})
