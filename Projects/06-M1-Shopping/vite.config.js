185a5e6ed741879b08a582cfc5c1cfd302a3f10b
Adam <adem.bekci.28@gmail.com>
2024-03-19 19:44:10 +0300

    vite config

--- Projects/06-M1-Shopping/vite.config.js
+++ Projects/06-M1-Shopping/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
+// import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base:"/06-M1-SHOPPING/"
+    // plugins: [react()],
+    base:"/JavaScript/Projects/06-M1-Shopping/"
 })
